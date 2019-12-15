d7021ee60fb3d75f448d03e1dee2a9ccca3cf561
juanfextlol <50813384+juanfextlol@users.noreply.github.com>
2019-12-15 00:10:23 -0500

    erre
    erre

--- src/index.js
+++ src/index.js
@@ -1,22 +1,35 @@
 const express = require("express");
 const path = require("path");
 const exphbs = require("express-handlebars");
+const methodOverride = require("method-override");
+const session = require("express-session");
 //Initializations
 const app = express();
 
 //settings
 app.set('port', process.env.PORT || 4000);
 app.set("views",path.join(__dirname,"views"));
-app.engine(".hbs",exphbs((
-defaultlayout:;
-layoutsDir:
-partialsDir:
-)));
+app.engine(".hbs",exphbs({
+defaultlayout:"main",
+layoutsDir:path.join(app.get('views'), 'layouts'),
+partialsDir:path.join(app.get('views'), 'partials'),
+extname: '.hbs'
+}));
+app.set("view engine",".hbs");
 //Midlewars 
-
+app.use(express.urlencoded({extended:false}));
+app.use(methodOverride("_method"));
+app.use(session({
+  secret:"mysecretapp",
+  resave:true,
+  saveUninitialized:true
+}));
 //Global Variables
 
 //Routes
+app.use(require("./routes/index"));
+app.use(require("./routes/notes"));
+app.use(require("./routes/users"));
 
 //Static Files
 
