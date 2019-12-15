c41dbaab0ceb9866281e19bde2a035c8bf90fb2c
juanfextlol <50813384+juanfextlol@users.noreply.github.com>
2019-12-15 00:33:53 -0500

    gtr
    erre

--- src/routes/notes.js
+++ src/routes/notes.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const router = express.Router();
-
+router.get("/perfil",(req, res) => {
+    res.send("Editar mi perfil");
+});
 
 module.exports=router;
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,5 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
-
+router.get("/users/signin",(req, res) => {
+    res.send("Ingresando a la Aplicacion");
+});
+router.get("/users/signup",(req, res) => {
+    res.send("Formulario de Autenticacion");
+});
 module.exports=router;
