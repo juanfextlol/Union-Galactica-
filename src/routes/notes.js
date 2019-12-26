6ff5f8d2cfa148624a512c425f0b5d1a287aa2aa
juanfextlol <50813384+juanfextlol@users.noreply.github.com>
2019-12-26 00:07:16 -0500

    hjjhj
    hjhjh

--- src/routes/notes.js
+++ src/routes/notes.js
@@ -5,6 +5,13 @@ router.get("/notes/add", (req,res)=>{
     res.render("notes/new-note");
 
 });
+router.post("/notes/new-note",(req, res)=>
+{
+    console.log(req.body);
+    res.send("ok");
+});
+
+
 router.get("/notes",(req, res) => {
     res.send("Editar mi perfil");
 });
