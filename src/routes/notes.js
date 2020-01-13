7755162a584427c53e2c1357d2c260d596554e4b
juanfextlol <50813384+juanfextlol@users.noreply.github.com>
2020-01-13 16:24:59 -0500

    UG-Actualizacion 0.0.6
    Actualizacion 0.0.6

--- src/routes/notes.js
+++ src/routes/notes.js
@@ -1,19 +1,66 @@
 const express = require("express");
 const router = express.Router();
 
+
+const Note = require('../models/Note');
+
 router.get("/notes/add", (req,res)=>{
     res.render("notes/new-note");
 
 });
-router.post("/notes/new-note",(req, res)=>
+router.post("/notes/new-note", async (req, res)=>
 {
     console.log(req.body);
-    res.send("ok");
+   const {title,description }= req.body;
+   const errors =[];
+   if (!title){
+    errors.push({text:"Porfavor Escriba el Titulo!"});
+
+   }
+   if (!description){
+       errors.push({text:"Porfavor Escriba una Descripcion"});
+
+   }
+   if (errors.length > 0 ){
+    res.render("notes/new-note",{
+        errors ,
+        title,
+        description
+    });
+    
+   } else {
+    
+   const newNote = new Note({title,description});
+   await newNote.save();
+   req.flash("success_msg","Torneo Agregado Sastifatoriamente!");
+   res.redirect("/notes");
+   }
+     
+
 });
 
 
-router.get("/notes",(req, res) => {
-    res.send("Editar mi perfil");
+router.get("/notes",async (req, res) => {
+
+    const notes = await Note.find().sort({date:"desc"});
+    res.render("notes/all-notes",{notes});
+
 });
 
+router.get("/notes/edit/:id", async (req, res) =>{
+    const note = await Note.findById(req.params.id);
+    res.render("notes/edit-note", {note});
+});
+
+router.put("/notes/edit-note/:id",async (req,res)=>{
+const {title,description}= req.body;
+     await Note.findByIdAndUpdate(req.params.id,{title ,description});
+     req.flash("success_msg","Torneo Actualizado Sastifatoriamente!");
+     res.redirect("/notes");
+});
+router.delete("/notes/delete/:id",async (req,res)=>{
+await Note.findByIdAndDelete(req.params.id)
+req.flash("success_msg","Torneo eliminado Sastifatoriamente!");
+res.redirect("/notes");
+});
 module.exports=router;
--- src/routes/users.js
+++ src/routes/users.js
@@ -7,4 +7,21 @@ router.get("/users/signin",(req, res) => {
 router.get("/users/signup",(req, res) => {
     res.render("users/signup");
 });
+router.post("/users/signup",(req,res)=>{
+    const {name,pais,email,password,confirm_password}= req.body;
+    const errors =[]
+    if(password != confirm_password){
+        errors.push({text:"Las Constraseñas No son iguales"});
+    }
+    if(password.lenght < 4 ){
+        errors.push({text:"La Contraseña debe ser mayor a 4 caracteres"
+        });
+    if(errors.length > 0){
+        res.render("users/signup",{errors,name,pais,email,password,confirm_password});
+    } else {
+        res.send("ok"); 
+
+    }
+    }
+});
 module.exports=router;
