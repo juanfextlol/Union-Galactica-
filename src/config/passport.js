59f9f906e0fe21e0a48f7085ad6c63097627fcc4
juanfextlol <50813384+juanfextlol@users.noreply.github.com>
2020-01-15 11:25:29 -0500

    weeaw
    ewawae

--- src/config/passport.js
+++ src/config/passport.js
@@ -1,30 +1,34 @@
-const passport = require("passport");
-const LocalStrategy = require("passport-local").Strategy;
-const User = require("../models/User");
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+
+const mongoose = require('mongoose');
+const User = require('../models/User'); 
 
 passport.use(new LocalStrategy({
-    usernameField:"email"
-
-},async(email,password,done)=>{
-    const user = await User.findOne({email: email});
-    if(!user){
-        return done (null,false,{message:"Usuario No encontrado"});
+  usernameField: 'email'
+}, async (email, password, done) => {
+  // Match Email's User
+  const user = await User.findOne({email: email});
+  if (!user) {
+    return done(null, false, { message: 'Not User found.' });
+  } else {
+    // Match Password's User
+    const match = await user.matchPassword(password);
+    if(match) {
+      return done(null, user);
     } else {
-       const match =  await User.matchPassword(password);
-       if(match){
-       return done(null,user);
+      return done(null, false, { message: 'Incorrect Password.' });
     }
-    else{
-        return done(null,false,{message:"Contraseña Incorrecta Chavo"});
-    }
-    }
+  }
 
 }));
-passport.serializeUser((user,done)=> {
-    done(null, user.id);
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
 });
-passport.deserializeUser((id,done) => {
-    User.findById(id,(err,user)=>{
-    done(err,user);
-    });
+
+passport.deserializeUser((id, done) => {
+  User.findById(id, (err, user) => {
+    done(err, user);
+  });
 });
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,47 +1,56 @@
-const express = require("express");
-const router = express.Router();
-const User =require("../models/User");
-const passport =require("passport");
+  
+const router = require('express').Router();
+const passport = require('passport');
 
-router.get("/users/signin",(req, res) => {
-    res.render("users/signin");
+// Models
+const User = require('../models/User');
+
+router.get('/users/signup', (req, res) => {
+  res.render('users/signup');
 });
-router.post("/users/signin ",passport.authenticate("local",{
-    successRedirect:"/notes",
-    failureRedirect:"/users/signin",
-    failureFlash:true
-}));
-router.get("/users/signup", (req, res) => {
-    res.render("users/signup");
-});
-router.post("/users/signup",async (req,res)=>{
-    const {name,pais,email,password,confirm_password}= req.body;
-    const errors =[];
-    if(name.length <= 0){
-        errors.push({text:"Porfavor Inserte un Nombre"});
-    }
-    if(password.length <= 0){
-        errors.push({text:"Porfavor Inserte una Contraseña"});
-    }
-    if(password != confirm_password){
-        errors.push({text:"Las Constraseñas No son iguales"});
-    }
-    if(password.lenght < 4 ){
-        errors.push({text:"La Contraseña debe ser mayor a 4 caracteres"});
-    }
-    if(errors.length > 0){
-        res.render("users/signup",{errors,name,pais,email,password,confirm_password});
-    } else {
+
+router.post('/users/signup', async (req, res) => {
+  let errors = [];
+  const { name, email, password, confirm_password } = req.body;
+  if(password != confirm_password) {
+    errors.push({text: 'Passwords do not match.'});
+  }
+  if(password.length < 4) {
+    errors.push({text: 'Passwords must be at least 4 characters.'})
+  }
+  if(errors.length > 0){
+    res.render('users/signup', {errors, name, email, password, confirm_password});
+  } else {
+    // Look for email coincidence
     const emailUser = await User.findOne({email: email});
-    if (emailUser ){
-        req.flash("error_msg","El Email ya esta en Uso ");
-        res.redirect("/users/signup");
+    if(emailUser) {
+      req.flash('error_msg', 'The Email is already in use.');
+      res.redirect('/users/signup');
+    } else {
+      // Saving a New User
+      const newUser = new User({name, email, password});
+      newUser.password = await newUser.encryptPassword(password);
+      await newUser.save();
+      req.flash('success_msg', 'You are registered.');
+      res.redirect('/users/signin');
     }
-    const newUser = new User({name,pais,email,password});
-    newUser.password = await newUser.encryptPassword(password);
-    await newUser.save();
-    req.flash("success_msg","Estas Registrado!")
-    res.redirect("/users/signin");
-    }
+  }
 });
-module.exports=router;
+
+router.get('/users/signin', (req, res) => {
+  res.render('users/signin');
+});
+
+router.post('/users/signin', passport.authenticate('local', {
+  successRedirect: '/notes',
+  failureRedirect: '/users/signin',
+  failureFlash: true
+}));
+
+router.get('/users/logout', (req, res) => {
+  req.logout();
+  req.flash('success_msg', 'You are logged out now.');
+  res.redirect('/users/signin');
+});
+
+module.exports = router;
